0b18d24e6c8b29ae80ba45dc2cc06e1eff46eefe
Mateus Daniel <35970984+MateusDanieel@users.noreply.github.com>
2020-01-29 14:57:40 -0300

    Finalização do desafio da semana 8.

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,51 +3,45 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-var sum = function calculateSum(x, y) {
+var sum = function calculateSum( x, y ) {
     return x + y;
-}; // ESQUECEU O ';'! NÃO PODE ESQUECER O PONTO E VIRGULA QUANDO ATRIBUI UMA FUNÇÃO A UMA VARIÁVEL
+};
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
+var a = 10;
+var b = 20;
 
-var a = 5;
-var b = 8;
-
-console.log('A soma de ' + a + ' e ' + b + ' é igual a ' + sum(a, b) + '.');
+console.log( 'A soma de ' + a + ' e ' + b + ' é igual a ' + sum(a, b) + '.' );
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-
-console.log( 'O nome da função que faz a soma é ' + sum.name + '.');
+console.log('O nome da função que faz a soma é ' + sum.name + '.');
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
 function showName() {
-    return 'Mateus';
+    return 'Mateus Daniel';
 }
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-var varShowName = showName(); // ERRADO!
-                              // AQUI ERA ESPERADO QUE RECEBE A FUNÇÃO, E NÃO A INVOCAÇÃO DA FUNÇÃO!
-                              // A RESPOSTA ESPERADA SERIA: 'var varShowName = showName'. REPARE QUE NÃO TEM O PARENTESES '()'
+var varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-console.log( 'A função ' + '"' + showName.name + '()"' + ' retorna ' + showName() + '.' ); // ERRADO!
-                                                                                           // AQUI ERA ESPERADO QUE MOSTRASSE O NOME DA FUNÇÃO E INVOCASSE A MESMA UTILIZANDO A VARIAVEL A QUAL ELA FOI ATRIBUIDA!
-                                                                                           // A RESPOSTA ESPERADA ERA: 'console.log( 'A função ' + varShowName.name + ' retorna ' + varShowName() + '.' );'
+console.log( 'A função ' + varShowName.name + ' retorna ' + varShowName() + '.' );
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -63,31 +57,31 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
 */
 function calculator(operator) {
 
-    return function calculate(x, y) {
-        var res;
+    return function(x, y) {
+        var result;
 
-        switch(operator) {
+        switch(operator){
             case '+':
-                res = x + y;
+                result = x + y;
                 break;
             case '-':
-                res = x - y;
+                result = x - y;
                 break;
             case '*':
-                res = x * y;
+                result = x * y;
                 break;
             case '/':
-                res = x / y;
+                result = x / y;
                 break;
             case '%':
-                res = x % y;
+                result = x % y;
                 break;
-            default: 
-                return "Operação inválida.";
+            default:
+                return 'Operação inválida.';
         }
 
-        return 'Resultado da operação: ' + x + ' ' + operator + ' ' + y + ' = ' + res + '.';
-    }; // ESQUECEU O PONTO E VIRGULA!
+        return 'Resultado da operação: ' + x + ' ' + operator + ' ' + y + ' ' + ' = ' + result + '.';
+    };
 
 }
 
@@ -100,7 +94,7 @@ var sum = calculator('+');
 /*
 Agora `sum` é uma função. Mostre no console a soma de dois números, usando ela.
 */
-sum(10,20); // 30 // ESQUECEU DE MOSTRAR NO CONSOLE!!
+console.log( sum(10,47) );
 
 /*
 Agora, declare algumas variáveis com os nomes `subtraction`, `multiplication`,
@@ -116,7 +110,7 @@ var mod = calculator('%');
 Faça uma operação com cada uma das funções criadas acima, mostrando o resultado
 no console.
 */
-console.log( subtraction(10, 5) ); // Resultado da operação: 10 - 5 = 5
-console.log( multiplication(20, 5) ); // Resultado da operação: 20 * 5 = 100
-console.log( division(16, 2) ); // Resultado da operação: 16 / 2 = 8
-console.log( mod(18, 3) ); // Resultado da operação: 18 % 3 = 0
+console.log( subtraction(10,8) );
+console.log( multiplication(20,7) );
+console.log( division(19,2) );
+console.log( mod(48,5) );
